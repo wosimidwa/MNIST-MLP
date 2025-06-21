42664fe29021791984bd2caadf19b7bf907fa213
wosimidwa <152811454+wosimidwa@users.noreply.github.com>
2025-06-21 14:29:17 +0300

    Update load_data.py

--- data/load_data.py
+++ data/load_data.py
@@ -43,3 +43,50 @@ class Dataloader(object):
        x_train, y_train = self.read_labels(self.train_img_filepath, self.train_labels_filepath)
        x_test, y_test = self.read_labels(self.test_img_filepath, self.test_labels_filepath)
        return (x_train, y_train), (x_test, y_test)
+
+# %% Verify Reading Dataset via DataLoader class
+import matplotlib.pyplot as plt
+import random
+
+input_path = '/home/wosi/Downloads/MNIST_archive/'
+training_img_filepath = join(input_path, 'train-images-idx3-ubyte/train-images-idx3-ubyte')
+training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte/train-labels-idx1-ubyte')
+test_img_filepath = join(input_path, 't10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
+test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
+
+#function to show a list of images with their relating titles
+
+def show_images(images, title):
+    cols = 5
+    rows = int(len(images)/cols) + 1
+    plt.figure(figsize=(30,20))
+    index = 1
+
+    for i in zip(images, title):
+        image = i[0]
+        title = i[1]
+        plt.subplot(rows, cols, index)
+        plt.imshow(image, cmap=plt.cm.gray)
+        if (title != ''):
+            plt.title(title, fontsize = 12)
+        index += 1
+
+# Load the dataset
+mnist_dataloader = Dataloader(training_img_filepath, training_labels_filepath, test_img_filepath, test_labels_filepath)
+(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+
+#show some random examples to check
+
+images_2_show = []
+titles_2_show = []
+for i in range(0, 10):
+    r = random.randint(1, 60000)
+    images_2_show.append(x_train[r])
+    titles_2_show.append('training image [' + str(r) + ']' + str(y_train[r]))
+
+for i in range(0, 5):
+    r = random.randint(1, 10000)
+    images_2_show.append(x_test[r])
+    titles_2_show.append('test image [' + str(r) + '] =' + str(y_test[r]))
+
+show_images(images_2_show, titles_2_show)
